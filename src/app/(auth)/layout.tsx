004722589aafb4681f6fe2b57a43ada6a5735e4b
kruddock <kevinruddock@gmail.com>
2025-03-30 17:47:23 +0000

    auth layout & associated pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,10 +2,12 @@ type AuthLayoutProps = {
   children: Readonly<React.ReactNode>
 }
 
-export default async function AuthLayout({ children }: AuthLayoutProps) {
+const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
       {children}
     </div>
   )
 }
+
+export default AuthLayout
