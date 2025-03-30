f96ab15cfe9b3f957ac968d937cf0e8154de3225
kruddock <kevinruddock@gmail.com>
2025-03-30 16:30:26 +0000

    clerk setup

--- src/envs/client.ts
+++ src/envs/client.ts
@@ -3,12 +3,16 @@ import * as v from 'valibot'
 
 export const env = createEnv({
   client: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: v.pipe(v.string(), v.minLength(1))
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: v.pipe(v.string(), v.minLength(1)),
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: v.pipe(v.string(), v.minLength(1)),
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: v.pipe(v.string(), v.minLength(1))
     // NEXT_PUBLIC_VARIABLE_NAME: v.pipe(v.string(), v.minLength(1))
   },
   runtimeEnv: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
-      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL
     // NEXT_PUBLIC_VARIABLE_NAME: process.env.NEXT_PUBLIC_VARIABLE_NAME
   }
 })
