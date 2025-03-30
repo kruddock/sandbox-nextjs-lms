03c02a7893b6c5e7d4259b3453973e691ba4ebfa
kruddock <kevinruddock@gmail.com>
2025-03-30 18:23:37 +0000

    update nav bar - show sign in button - hide nav links (reserved for authenticated users)

--- src/app/(consumer)/layout.tsx
+++ src/app/(consumer)/layout.tsx
@@ -1,5 +1,8 @@
 import Link from 'next/link'
 import { Logo } from '@/components/Logo'
+import { Suspense } from 'react'
+import { SignedIn, SignedOut, SignInButton } from '@clerk/nextjs'
+import { Button } from '@/components/ui/button'
 
 type ConsumerLayoutProps = {
   children: Readonly<React.ReactNode>
@@ -17,10 +20,31 @@ const ConsumerLayout = ({ children }: ConsumerLayoutProps) => {
 const Navbar = () => {
   return (
     <header className="h-16 shadow bg-background z-10">
-      <nav className="p-4 grid grid-cols-[auto_1fr] gap-4">
+      <nav className="p-4 grid grid-cols-[auto_1fr_auto] gap-4 items-center">
         <Link href="/">
           <Logo />
         </Link>
+
+        <div className="flex gap-4">
+          <Suspense>
+            <SignedIn>
+              <Link className="hover:text-blue-400" href="/courses">
+                My Courses
+              </Link>
+              <Link className="hover:text-blue-400" href="/purchases">
+                Purchase History
+              </Link>
+            </SignedIn>
+          </Suspense>
+        </div>
+
+        <Suspense>
+          <SignedOut>
+            <Button asChild>
+              <SignInButton>Sign In</SignInButton>
+            </Button>
+          </SignedOut>
+        </Suspense>
       </nav>
     </header>
   )
