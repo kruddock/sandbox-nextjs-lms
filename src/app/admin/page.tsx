40863fcecdb39dac021c724d6d0cb409ca07347f
kruddock <kevinruddock@gmail.com>
2025-03-30 09:29:30 +0000

    admin page (wip)

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,15 @@
+export default async function AdminPage() {
+  return (
+    <div className="container my-6">
+      <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 md:grid-cols-4 gap-4">
+        <div>Stat Card #1</div>
+
+        <div>Stat Card #2</div>
+
+        <div>Stat Card #3</div>
+
+        <div>Stat Card #4</div>
+      </div>
+    </div>
+  )
+}
