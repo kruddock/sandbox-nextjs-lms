b5e5c4149186e935d1bc66e2b65478b2e4998022
kruddock <kevinruddock@gmail.com>
2025-04-01 03:06:04 +0000

    rename db methods

--- src/features/products/db/index.ts
+++ src/features/products/db/index.ts
@@ -1,6 +1,6 @@
-import { CourseProductTable, ProductTable } from '@/drizzle/schema'
 import { db } from '@/drizzle/db'
 import { eq } from 'drizzle-orm'
+import { CourseProductTable, ProductTable } from '@/drizzle/schema'
 import { revalidateProductCache } from '../cache'
 
 type ProductInsert = typeof ProductTable.$inferInsert & { courseIds: string[] }
@@ -8,9 +8,21 @@ type ProductUpdate = Partial<typeof ProductTable.$inferInsert> & {
   courseIds: string[]
 }
 
-export const storeProduct = async (payload: ProductInsert) => {
+export const findById = async (id: string) => {
+  const [product] = await db
+    .select()
+    .from(ProductTable)
+    .where(eq(ProductTable.id, id))
+
+  if (product == null) throw new Error('Failed to find product')
+
+  return product
+}
+
+export const store = async (payload: ProductInsert) => {
   const newId = await db.transaction(async (tx) => {
     const { courseIds, ...data } = payload
+
     const [{ id }] = await tx.insert(ProductTable).values(data).$returningId()
 
     if (id == null) {
@@ -28,12 +40,14 @@ export const storeProduct = async (payload: ProductInsert) => {
     return id
   })
 
-  revalidateProductCache(newId)
+  const newProduct = await findById(newId)
 
-  return newId
+  revalidateProductCache(newProduct.id)
+
+  return newProduct
 }
 
-export const updateProduct = async (id: string, payload: ProductUpdate) => {
+export const update = async (id: string, payload: ProductUpdate) => {
   await db.transaction(async (tx) => {
     const { courseIds, ...data } = payload
 
@@ -51,15 +65,19 @@ export const updateProduct = async (id: string, payload: ProductUpdate) => {
     )
   })
 
-  revalidateProductCache(id)
+  const updatedProduct = await findById(id)
 
-  return id
+  revalidateProductCache(updatedProduct.id)
+
+  return updatedProduct
 }
 
-export const deleteProduct = async (id: string) => {
+export const remove = async (id: string) => {
+  const deletedProduct = await findById(id)
+
   await db.delete(ProductTable).where(eq(ProductTable.id, id))
 
-  revalidateProductCache(id)
+  revalidateProductCache(deletedProduct.id)
 
-  return id
+  return deletedProduct
 }
