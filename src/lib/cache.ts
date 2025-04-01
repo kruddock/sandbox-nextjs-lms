a9ffe30804911a8af5dc645f6db03ff7151583c6
kruddock <kevinruddock@gmail.com>
2025-04-01 03:06:41 +0000

    additional cache tag methods

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,13 +1,15 @@
-type CACHE_TAG = 'products' | 'users'
+type CACHE_TAG = 'products' | 'users' | 'courses'
 
 export const getGlobalTag = (tag: CACHE_TAG) => `global:${tag}` as const
 
 export const getIdTag = (tag: CACHE_TAG, id: string) =>
   `id:${id}-${tag}` as const
 
-export const getUserTag = (tag: CACHE_TAG, userId: string) => `user:${userId}-${tag}` as const
+export const getUserTag = (tag: CACHE_TAG, userId: string) =>
+  `user:${userId}-${tag}` as const
 
-export const getCourseTag = (tag: CACHE_TAG, courseId: string) => `course:${courseId}-${tag}` as const
+export const getCourseTag = (tag: CACHE_TAG, courseId: string) =>
+  `course:${courseId}-${tag}` as const
 
 // export function getGlobalTag(tag: CACHE_TAG) {
 //   return `global:${tag}` as const
