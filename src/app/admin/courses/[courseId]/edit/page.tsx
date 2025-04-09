e1dd7836b21d5e64b476d3531975b2535a5acb70
kruddock <kevinruddock@gmail.com>
2025-04-09 21:47:47 +0000

    display course sections (wip)

--- src/app/admin/courses/[courseId]/edit/page.tsx
+++ src/app/admin/courses/[courseId]/edit/page.tsx
@@ -1,8 +1,13 @@
 import { PageHeader } from '@/components/PageHeader'
-import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { DialogTrigger } from '@/components/ui/dialog'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { getCourse } from '@/features/courses/action'
 import { CourseForm } from '@/features/courses/components/CourseForm'
+import { SectionFormDialog } from '@/features/courseSections/components/SectionFormDialog'
+import { cn } from '@/lib/utils'
+import { EyeClosed, PlusIcon } from 'lucide-react'
 import { notFound } from 'next/navigation'
 
 type EditCoursePageProps = {
@@ -18,7 +23,7 @@ const EditCoursePage = async ({ params }: EditCoursePageProps) => {
 
   return (
     <div className="container my-6">
-      <PageHeader title="New Course" />
+      <PageHeader title={course.name} />
       <Tabs defaultValue="lessons">
         <TabsList>
           <TabsTrigger value="lessons">Lessons</TabsTrigger>
@@ -27,10 +32,39 @@ const EditCoursePage = async ({ params }: EditCoursePageProps) => {
 
         <TabsContent value="lessons" className="flex flex-col gap-2">
           <Card>
-            <CardHeader>Card Heading</CardHeader>
+            <CardHeader className="flex items-center flex-row justify-between">
+              <CardTitle>Sections</CardTitle>
+              <SectionFormDialog courseId={course.id}>
+                <DialogTrigger asChild>
+                  <Button>
+                    <PlusIcon /> New Section
+                  </Button>
+                </DialogTrigger>
+              </SectionFormDialog>
+            </CardHeader>
 
-            <CardContent>Card Content</CardContent>
+            <CardContent>
+              <SortableSectionList
+                courseId={course.id}
+                sections={course.courseSections}
+              />
+            </CardContent>
           </Card>
+
+          <hr className="my-2" />
+
+          {course.courseSections.map((section) => (
+            <Card key={section.id}>
+              <CardTitle
+                className={cn(
+                  'flex items-center gap-2',
+                  section.status === 'private' && 'text-muted-foreground'
+                )}
+              >
+                {section.status === 'private' && <EyeClosed />} {section.name}
+              </CardTitle>
+            </Card>
+          ))}
         </TabsContent>
 
         <TabsContent value="details" className="flex flex-col gap-2">
