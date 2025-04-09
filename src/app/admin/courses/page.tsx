74772497936cad28f09aeee87af8326ba8107206
kruddock <kevinruddock@gmail.com>
2025-04-09 16:46:11 +0000

    update route

--- src/app/admin/courses/page.tsx
+++ src/app/admin/courses/page.tsx
@@ -1,14 +1,14 @@
-import { PageHeader } from '@/components/PageHeader'
-import { Button } from '@/components/ui/button'
-import { CourseTable } from '@/features/courses/components/CourseTable'
 import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { PageHeader } from '@/components/PageHeader'
+import { CourseTable } from '@/features/courses/components/CourseTable'
 
 const CoursesPage = async () => {
   return (
     <div className="">
       <PageHeader title="Courses">
         <Button asChild>
-          <Link href="/admin/courses/new">New Course</Link>
+          <Link href="/admin/courses/create">New Course</Link>
         </Button>
       </PageHeader>
 
